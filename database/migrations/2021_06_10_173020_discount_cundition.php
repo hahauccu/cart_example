80d3d4bb16a39572b3284f754288189d938e984e
victor <zxc25852ad40@gmail.com>
2021-06-11 02:10:25 +0800

    fix discount migrate file

--- database/migrations/2021_06_10_173020_discount_cundition.php
+++ database/migrations/2021_06_10_173020_discount_cundition.php
@@ -15,7 +15,7 @@ class DiscountCundition extends Migration
     {
         Schema::create('discount_cundition', function (Blueprint $table) {
             $table->id();
-            $table->text('discountCode');
+            $table->text('discount_code');
             $table->text('discount_content');
             $table->integer('type');
             $table->integer('price_condition');
@@ -26,21 +26,21 @@ class DiscountCundition extends Migration
         DB::table('discount_cundition')->insert(
         array(
                 [
-                    'discountCode' => "tenpErcentOff",
+                    'discount_code' => "tenpErcentOff",
                     'type' => "1",
                     'price_condition' => "40000",
                     'price' => "0.9",
                     "discount_content"=>"if check price bigger than condition than get ten percent off"
                 ],
                 [
-                    'discountCode' => "buy30000get1000Back",
+                    'discount_code' => "buy30000get1000Back",
                     'type' => "2",
                     'price_condition' => "30000",
                     'price' => "1000",
                     "discount_content"=>"if check price bigger than condition than get 1000 back"
                 ],
                 [
-                    'discountCode' => "buy60000get3000Back",
+                    'discount_code' => "buy60000get3000Back",
                     'type' => "2",
                     'price_condition' => "60000",
                     'price' => "3000",
