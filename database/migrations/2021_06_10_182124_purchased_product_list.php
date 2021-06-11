6e70644111e55bedc897d2cb001e5f1c0024a0b4
victor <zxc25852ad40@gmail.com>
2021-06-11 10:44:34 +0800

    add check out migrate file

---
+++ database/migrations/2021_06_10_182124_purchased_product_list.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PurchasedProductList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchased_product_list', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('purchased_number');
+            $table->text('check_out_id');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchased_product_list');
+    }
+}
